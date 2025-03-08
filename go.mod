b5e2bf8c3a81f1513ea198e07fdd6636e0e9c5c2
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-03-08 14:35:47 -0500

    API to count site hits
    - apiConfig struct to hold state
    - limited endpoints to specific methods
    - logging and tracking middleware defined and implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GianniBuoni/chirpy
 
 go 1.23.4
+
+require github.com/skodnik/go-contenttype v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
+
+	"github.com/skodnik/go-contenttype/contenttype"
 )
 
 func main() {
@@ -10,10 +14,17 @@ func main() {
 		port         string = "8080"
 		filePathRoot string = "."
 	)
+	// api config
+	api := new(apiConfig)
+
 	// handlers
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", healthCheck)
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.HandleFunc("GET /healthz", healthCheck)
+	mux.Handle("/app/", api.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))),
+	))
+	mux.HandleFunc("GET /metrics", api.handleMetrics)
+	mux.HandleFunc("POST /reset", api.handleReset)
 
 	// init server
 	server := new(http.Server)
@@ -25,8 +36,40 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
+const ()
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		log.Print(cfg.hits())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) hits() string {
+	return fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", contenttype.TextPlain)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(cfg.hits()))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	log.Print(cfg.hits())
+	w.Header().Add("Content-Type", contenttype.TextPlain)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(cfg.hits()))
+}
+
 func healthCheck(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", contenttype.TextPlain)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK\n"))
 }
